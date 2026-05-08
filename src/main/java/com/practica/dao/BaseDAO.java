757ef0cec70bf16fa3aa0418e1ed4f0ac43bc3be
Patricia <trishawx008@gmail.com>
2026-05-08 13:35:11 +0300

    Mostenirea tuturor claselor Data Acces Oject

--- src/main/java/com/practica/dao/BaseDAO.java
+++ src/main/java/com/practica/dao/BaseDAO.java
@@ -1,5 +1,19 @@
 package com.practica.dao;
 
-public class BaseDAO {
-    
+import com.practica.database.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public abstract class BaseDAO<T> {
+    protected Connection connection;
+
+    public BaseDAO() throws SQLException {
+        this.connection = DatabaseConnection.getConnection();
+    }
+
+    public abstract void add(T obj) throws SQLException;
+    public abstract List<T> getAll() throws SQLException;
+    public abstract void update(T obj) throws SQLException;
+    public abstract void delete(int id) throws SQLException;
 }
